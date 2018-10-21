b19c15c77467c221eb07f3bcfefe263168434d15
Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>
2018-10-21 16:28:58 +0200

    Compiler Attributes: auxdisplay: panel: use __fallthrough
    Let gcc know these cases are meant to fall through to the next label
    by annotating them with the new __fallthrough statement attribute;
    and remove the comment since it conveys the same information
    (which was also parsed by gcc to suppress the warning).
    
    Signed-off-by: Miguel Ojeda <miguel.ojeda.sandonis@gmail.com>

--- drivers/auxdisplay/panel.c
+++ drivers/auxdisplay/panel.c
@@ -1367,7 +1367,7 @@ static void panel_process_inputs(void)
 				break;
 			input->rise_timer = 0;
 			input->state = INPUT_ST_RISING;
-			/* fall through */
+			__fallthrough;
 		case INPUT_ST_RISING:
 			if ((phys_curr & input->mask) != input->value) {
 				input->state = INPUT_ST_LOW;
@@ -1380,11 +1380,11 @@ static void panel_process_inputs(void)
 			}
 			input->high_timer = 0;
 			input->state = INPUT_ST_HIGH;
-			/* fall through */
+			__fallthrough;
 		case INPUT_ST_HIGH:
 			if (input_state_high(input))
 				break;
-			/* fall through */
+			__fallthrough;
 		case INPUT_ST_FALLING:
 			input_state_falling(input);
 		}
