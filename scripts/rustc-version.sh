89a9dc6bbb71a3ea09348f64dc2b2df8431c0fd2
Gary Guo <gary@garyguo.net>
2024-10-10 09:38:26 +0000

    kbuild: rust: add `CONFIG_RUSTC_LLVM_VERSION`
    Each version of Rust supports a range of LLVM versions. There are cases where
    we want to gate a config on the LLVM version instead of the Rust version.
    Normalized cfi integer tags are one example [1].
    
    For consistency with cc-version and ld-version, the new version number is added
    to the existing rustc-version script, rather than being added to a new script.
    
    The invocation of rustc-version is being moved from init/Kconfig to
    scripts/Kconfig.include to avoid invoking rustc-version.sh twice and for
    consistency with cc-version.
    
    Link: https://lore.kernel.org/all/20240925-cfi-norm-kasan-fix-v1-1-0328985cdf33@google.com/ [1]
    Signed-off-by: Gary Guo <gary@garyguo.net>
    Signed-off-by: Alice Ryhl <aliceryhl@google.com>
    Link: https://lore.kernel.org/r/20241010-icall-detect-vers-v1-1-8f114956aa88@google.com
    Signed-off-by: Miguel Ojeda <ojeda@kernel.org>

--- scripts/rustc-version.sh
+++ scripts/rustc-version.sh
@@ -3,14 +3,23 @@
 #
 # Usage: $ ./rustc-version.sh rustc
 #
-# Print the Rust compiler version in a 6 or 7-digit form.
+# Print the Rust compiler version and the LLVM version it uses in a 6 or
+# 7-digit form.
+
+# Convert the version string x.y.z to a canonical up-to-6-digits form.
+get_llvm_canonical_version()
+{
+	IFS=.
+	set -- $1
+	echo $((10000 * $1 + 100 * $2 + $3))
+}
 
 # Convert the version string x.y.z to a canonical up-to-7-digits form.
 #
-# Note that this function uses one more digit (compared to other
-# instances in other version scripts) to give a bit more space to
+# Note that this function uses one more digit (compared to other instances in
+# other version scripts and the instance above) to give a bit more space to
 # `rustc` since it will reach 1.100.0 in late 2026.
-get_canonical_version()
+get_rustc_canonical_version()
 {
 	IFS=.
 	set -- $1
@@ -19,8 +28,18 @@ get_canonical_version()
 
 if output=$("$@" --version 2>/dev/null); then
 	set -- $output
-	get_canonical_version $2
+	rustc_version=$(get_rustc_canonical_version $2)
 else
-	echo 0
+	echo 0 0
 	exit 1
 fi
+
+if output=$("$@" --version --verbose 2>/dev/null | grep LLVM); then
+	set -- $output
+	rustc_llvm_version=$(get_llvm_canonical_version $3)
+else
+	echo 0 0
+	exit 1
+fi
+
+echo $rustc_version $rustc_llvm_version
